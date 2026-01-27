f5ea323299e5904d20d1223fa8da8bed80324356
Thanh Danh <thanhdanh431@gmail.com>
2026-01-27 21:38:22 +0700

    refactor(license): simplify TeacherLicenseTypeConfiguration setup

--- DrivingLicense.Infrastructure/Data/Configurations/TeacherLicenseTypeConfiguration.cs
+++ DrivingLicense.Infrastructure/Data/Configurations/TeacherLicenseTypeConfiguration.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using DrivingLicense.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -14,9 +10,7 @@ namespace DrivingLicense.Infrastructure.Data.Configurations
         {
             builder.ToTable("TeacherLicenseTypes");
 
-            builder.HasKey(tlt => tlt.Id);
-
-            builder.HasIndex(tlt => new { tlt.TeacherId, tlt.LicenseTypeId }).IsUnique();
+            builder.HasKey(tlt => new { tlt.TeacherId, tlt.LicenseTypeId });
 
             builder.HasOne(tlt => tlt.Teacher)
                    .WithMany(t => t.TeacherLicenseTypes)
