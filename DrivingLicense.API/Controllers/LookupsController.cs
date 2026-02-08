a903de28f740732ec95aa5c9ade018ef54f59d19
Thanh Danh <thanhdanh431@gmail.com>
2026-02-08 21:13:54 +0700

    feat(controller): add LookupsController for license types

---
+++ DrivingLicense.API/Controllers/LookupsController.cs
@@ -0,0 +1,31 @@
+﻿using DrivingLicense.Application.Common.ApiResponses;
+using DrivingLicense.Application.DTOs.Common;
+using DrivingLicense.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrivingLicense.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LookupsController : ControllerBase
+    {
+        private readonly ILicenseTypeService _licenseTypeService;
+        private readonly ICourseService _courseService;
+
+        public LookupsController(
+            ILicenseTypeService licenseTypeService,
+            ICourseService courseService
+           )
+        {
+            _licenseTypeService = licenseTypeService;
+            _courseService = courseService;
+        }
+
+        [HttpGet("license-types")]
+        public async Task<IActionResult> GetLicenseTypes()
+        {
+            var licenseTypes = await _licenseTypeService.GetDropDownListAsync();
+            return Ok(ApiResponse<IEnumerable<LookupDto>>.SuccessResponse(licenseTypes));
+        }
+    }
+}
