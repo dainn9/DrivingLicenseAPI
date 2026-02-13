2a6996e8fcddaebb06db57d6b56b92e99f82f65d
Thanh Danh <thanhdanh431@gmail.com>
2026-02-13 10:20:42 +0700

    feat(auth): add RefreshToken class for managing refresh tokens

---
+++ DrivingLicense.Infrastructure/Authentication/RefreshToken.cs
@@ -0,0 +1,14 @@
+﻿namespace DrivingLicense.Infrastructure.Authentication
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+        public AppUser User { get; set; } = null!;
+        public DateTime ExpiresAt { get; set; }
+        public bool IsRevoked { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? RevokedAt { get; set; }
+    }
+}
