0c4809db17f2ff0532eb54b742f623e865c58b12
Thanh Danh <thanhdanh431@gmail.com>
2026-02-13 10:15:52 +0700

    feat(user): add UserDto for user data transfer

---
+++ DrivingLicense.Application/DTOs/User/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DrivingLicense.Application.DTOs.User
+{
+    public class UserDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public List<string> Role { get; set; } = new List<string>();
+    }
+}
