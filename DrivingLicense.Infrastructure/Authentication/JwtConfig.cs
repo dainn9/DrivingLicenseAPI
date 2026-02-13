ce81d554ecb59d7eaa41f9eb15828786fe9f166e
Thanh Danh <thanhdanh431@gmail.com>
2026-02-13 10:18:40 +0700

    feat(auth): add JwtConfig class for JWT configuration settings

---
+++ DrivingLicense.Infrastructure/Authentication/JwtConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace DrivingLicense.Infrastructure.Authentication
+{
+    public class JwtConfig
+    {
+        public string Secret { get; set; } = string.Empty;
+        public string ValidIssuer { get; set; } = string.Empty;
+        public string ValidAudience { get; set; } = string.Empty;
+        public int AccessTokenExpirationMinutes { get; set; }
+        public int RefreshTokenExpirationDays { get; set; }
+    }
+}
