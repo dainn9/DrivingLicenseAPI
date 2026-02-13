c7b7652d3e08916e3c0a4230ff49b6d7404ea5aa
Thanh Danh <thanhdanh431@gmail.com>
2026-02-13 10:23:48 +0700

    refactor(auth): remove unused using directives in AppRoles

--- DrivingLicense.Infrastructure/Authentication/AppRoles.cs
+++ DrivingLicense.Infrastructure/Authentication/AppRoles.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DrivingLicense.Infrastructure.Authentication
+﻿namespace DrivingLicense.Infrastructure.Authentication
 {
     public static class AppRoles
     {
